c49f6375c5ba23d80ed0da7e2b56402a82cd6369
秦昀 <954800703@qq.com>
2020-12-27 18:07:56 +0800

    完成了项目结构的初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <router-view></router-view>
+    <h1>头条项目</h1>
+    <div>
+      <i class="toutiao toutiao-dianzan2"></i>
     </div>
-    <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    name: 'App'
   }
-}
+</script>
+<style lang="less">
+  .box {
+    width: 750px;
+    height: 60px;
+    background-color: pink;
+  }
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+// 请求拦截器
+// 相应拦截器
+export default request
